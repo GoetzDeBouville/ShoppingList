6a23c0ec761137e2555750b15a94c0e1b3d38839
GoetzDeBouville <31970713+GoetzDeBouville@users.noreply.github.com>
2023-06-26 18:04:48 +0300

    Исправлены баги сборки

--- app/src/main/java/com/hellcorp/shoppinglist/activities/MainActivity.kt
+++ app/src/main/java/com/hellcorp/shoppinglist/activities/MainActivity.kt
@@ -2,6 +2,7 @@ package com.hellcorp.shoppinglist.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hellcorp_studio.shoppinglist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
