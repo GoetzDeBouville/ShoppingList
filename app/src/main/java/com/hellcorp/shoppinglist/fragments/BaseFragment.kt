b6b66f29af1f92d52a7988893057742c4875efef
GoetzDeBouville <31970713+GoetzDeBouville@users.noreply.github.com>
2023-06-28 13:52:54 +0300

    Добавлен базовый фрагмент. Прописан FragmentManager, который будет производить замену записи в placeholder

---
+++ app/src/main/java/com/hellcorp/shoppinglist/fragments/BaseFragment.kt
@@ -0,0 +1,7 @@
+package com.hellcorp.shoppinglist.fragments
+
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment : Fragment() {
+    abstract fun onClickNew()
+}
---
+++ app/src/main/java/com/hellcorp/shoppinglist/fragments/FragmentManager.kt
@@ -0,0 +1,15 @@
+package com.hellcorp.shoppinglist.fragments
+
+import androidx.appcompat.app.AppCompatActivity
+import com.hellcorp_studio.shoppinglist.R
+
+object FragmentManager {
+    var currentFragment: BaseFragment? = null
+
+    fun setFragment(newFragment: BaseFragment, activity: AppCompatActivity) {
+        val transaction = activity.supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.placeholder, newFragment)
+        transaction.commit()
+        currentFragment = newFragment
+    }
+}
